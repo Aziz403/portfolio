db3a51d58f7af0aa741ec5af4239badf5f284a5f
aziz benmallouk <azizbenmallouk4@gmail.com>
2023-02-08 13:04:45 +0000

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+let isDevelopment = true;
+
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?[c]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.sass$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: { indentedSyntax: true },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|woff|woff2|eot|ttf|svg)$/i,
+        use: 'url-loader?limit=1024',
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    })
+  ]
+};
